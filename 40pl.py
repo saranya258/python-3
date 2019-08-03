4891ebe8310e868d0fb0b54d922b5d00554a9666
Saranya Kumar <51949493+saranya258@users.noreply.github.com>
2019-08-03 13:47:08 +0530

    Update 40pl.py

--- 40pl.py
+++ 40pl.py
@@ -1,7 +1,34 @@
-i=int(input())
-if(i==10):
-  print("6")
-elif(i==5):
-  print("3")
-else:
-  print("2")
+def multiply(F, M): 
+
+	x = F[0][0] * M[0][0] + F[0][1] * M[1][0] 
+	y = F[0][0] * M[0][1] + F[0][1] * M[1][1] 
+	z = F[1][0] * M[0][0] + F[1][1] * M[1][0] 
+	w = F[1][0] * M[0][1] + F[1][1] * M[1][1] 
+
+	F[0][0] = x 
+	F[0][1] = y 
+	F[1][0] = z 
+	F[1][1] = w 
+
+def power(F, n): 
+
+	if( n == 0 or n == 1): 
+		return
+	M = [[1, 1],[1, 0]] 
+
+	power(F, n // 2) 
+	multiply(F, F) 
+
+	if (n % 2 != 0): 
+		multiply(F, M) 
+def countWays(n): 
+	F = [[1, 1], [1, 0]] 
+	if (n == 0): 
+		return 0
+	power(F, n) 
+
+	return F[0][0] 
+
+# Driver Code 
+n = int(input())
+print(countWays(n)) 
